b5d5b2228d1cb5b76b196847c6df623c9aada61b
naonxj <cc4391@naver.com>
2024-05-05 02:58:12 +0900

    modify router

--- app/(screens)/_layout.tsx
+++ app/(screens)/_layout.tsx
@@ -5,6 +5,9 @@ import { useAuth0 } from "react-native-auth0";
 import HeaderComponent from "@/components/HeaderComponent";
 import MainHeader from "@/components/MainHeader";
 import TodoScreen from "./todo";
+import colors from "@/constants/Colors";
+import { Header } from "react-native/Libraries/NewAppScreen";
+
 export default function Layout() {
   const { clearSession, clearCredentials, user, isLoading } = useAuth0();
 
@@ -35,10 +38,16 @@ export default function Layout() {
 
   return(
     <Stack 
-      screenOptions={{header: MainHeader}}
+      initialRouteName=""
+      screenOptions={{
+        header: MainHeader,
+        headerStyle:{backgroundColor: colors.lightgray.background}
+      }}
     >
+    {/* <Stack.Screen name="schedule" options={{header: HeaderComponent}}/> */}
     <Stack.Screen name="todo" options={{header: HeaderComponent}}/>
-
+    {/* <Stack.Screen name="Comment" options={{header:HeaderComponent}}/> */}
+    {/* <Stack.Screen name="challenge" options={{header: HeaderComponent}}/> */}
     </Stack>
   );
   
