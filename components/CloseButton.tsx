54fa08bfe244e955bca12d61757d55eedf7e2b3d
naonxj <cc4391@naver.com>
2024-05-05 03:00:22 +0900

    modify styles

--- components/CloseButton.tsx
+++ components/CloseButton.tsx
@@ -7,11 +7,9 @@ export default function LoginButton(props: any) {
     const closeicon=require('@/assets/images/x.png')
   return (
     <View style={styles.container}>
-      <Link href="/" asChild>
         <Pressable onPress={props.onPress}>
           <Image style={styles.button}  source={closeicon}/>
         </Pressable>
-      </Link>
     </View>
   );
 }
@@ -20,9 +18,10 @@ const styles = StyleSheet.create({
   container: {
     marginTop:"5%",
     marginLeft:"5%",
+    marginBottom:"5%",
   },
   button: {
-    width: 35,
-    height: 35,
+    width: 30,
+    height: 30,
   },
 });
