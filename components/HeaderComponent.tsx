0251af897f1777ccfcfad32312e9f781b7bd31f9
naonxj <cc4391@naver.com>
2024-05-05 03:01:14 +0900

    modify onPress

--- components/HeaderComponent.tsx
+++ components/HeaderComponent.tsx
@@ -1,40 +1,32 @@
 import { StyleSheet, Text,View } from "react-native";
 import CloseButton from "@/components/CloseButton";
 import CheckButtion from "@/components/CheckButton";
-import { Link } from "expo-router";
+import { Link , useRouter} from "expo-router";
 import colors from "@/constants/Colors";
+import Grayline from "@/components/Grayline";
 // import { router} from "expo-router";
-
 export default function HeaderComponent(props: any)  {
+  const router=useRouter();
   return (
     <>
-      <View style={styles.container}>
-        {/* <View style={styles.linestyle}>
-          <View style={styles.line}/>
-        </View> */}
-          <CloseButton/>
-          <CheckButtion onPress={{}} />
+      <View style={styles.content}>
+        <Grayline/>
+        <View style={styles.container}>
+          <CloseButton onPress={()=> router.back()} />
+          <CheckButtion onPress={()=> router.push("/(screens)/")} />
+        </View>
       </View>
     </>
   );
 }
 
 const styles = StyleSheet.create({
+  content:{
+    flexDirection:"column",
+  },
   container:{
     flexDirection:"row",
     justifyContent:"space-between",
     alignItems:"center",
 },
-  linestyle:{
-    alignItems:"center",
-    justifyContent:"center",
-  },
-  line:{
-    width:130,
-    height:11,
-    // backgroundColor:colors.lightgray.background,
-    backgroundColor:"Red",
-    borderRadius:15,
-    marginTop:5,
-  },
 });
