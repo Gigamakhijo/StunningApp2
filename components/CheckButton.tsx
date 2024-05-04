9f4efad6ee7cb783bdaf866e3fcf34f5ddf3e9d8
naonxj <cc4391@naver.com>
2024-05-05 03:00:13 +0900

    modify styles

--- components/CheckButton.tsx
+++ components/CheckButton.tsx
@@ -7,11 +7,9 @@ export default function LoginButton(props: any) {
     const checkicon=require('@/assets/images/checkicon.png')
   return (
     <View style={styles.container}>
-      <Link href="/" asChild>
         <Pressable onPress={props.onPress}>
           <Image style={styles.button}  source={checkicon}/>
         </Pressable>
-      </Link>
     </View>
   );
 }
@@ -20,9 +18,10 @@ const styles = StyleSheet.create({
   container: {
     marginTop:"5%",
     marginRight:"5%",
+    marginBottom:"5%",
   },
   button: {
-    width: 35,
-    height: 35,
+    width: 30,
+    height: 30,
   },
 });
