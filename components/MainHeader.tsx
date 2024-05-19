a7535ae10b472b88629fc1aa0fced7657b34def5
naonxj <cc4391@naver.com>
2024-05-20 04:53:02 +0900

    modify router

--- components/MainHeader.tsx
+++ components/MainHeader.tsx
@@ -2,18 +2,24 @@ import { StyleSheet, Text,View ,Image, Pressable} from "react-native";
 import { Link } from "expo-router";
 // import { router} from "expo-router";
 import SmallButton from "@/components/SmallButton";
+import { useRouter } from "expo-router";
+import icon from "@/constants/Icon";
+import { Drawer } from "expo-router/drawer";
 
-export default function HeaderComponent(props: any)  {
-    const listicon=require('@/assets/images/listicon.png')
+export default function MainHeader()  {
     const testpress=()=>{
       console.log("hi")
+    };
+    const router=useRouter();
+    const openDrawer=()=>{
+
     };
   return (
     <>
       <View style={styles.container}>
         <SmallButton href="./chat" text="Trainer" />
             <Pressable style={styles.buttonstyle} onPress={testpress}>
-              <Image style={styles.button}  source={listicon}/>
+              <Image style={styles.button}  source={icon.listicon}/>
             </Pressable>
       </View>
     </>
