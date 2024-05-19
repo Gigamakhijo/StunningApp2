9d47e24659b79ebc1c94f3710dca5f488d60add2
naonxj <cc4391@naver.com>
2024-05-20 04:50:44 +0900

    add chat screen

---
+++ app/(screens)/chat.tsx
@@ -0,0 +1,19 @@
+import { StyleSheet, Text } from "react-native";
+import Logo from "@/components/Logo";
+import TextField from "@/components/TextField";
+import CancelButton from "@/components/CancelButton";
+import ConfirmButton from "@/components/ConfirmButton";
+
+export default function LoginScreen() {
+  return (
+    <>
+      <Text style={styles.textstyle}>trainer page</Text>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  textstyle:{
+    marginTop:"5%",
+  },
+});
