74bdc2949cd6f345f2025bd30cf41a5cb703ba52
naonxj <cc4391@naver.com>
2024-05-20 04:58:15 +0900

    add router route

--- app/(screens)/_layout.tsx
+++ app/(screens)/_layout.tsx
@@ -1,12 +1,16 @@
 import { Redirect, Stack } from "expo-router";
-import { Alert, Button, StyleSheet, Text, View } from "react-native";
-
+import { Alert, Button, StyleSheet, Text, View, Image,} from "react-native";
+import {Drawer } from 'expo-router/drawer';
+import {Tabs} from 'expo-router/tabs';
 import { useAuth0 } from "react-native-auth0";
 import HeaderComponent from "@/components/HeaderComponent";
 import MainHeader from "@/components/MainHeader";
-import TodoScreen from "./todo";
+import tabs from "./(tabs)/todo";
 import colors from "@/constants/Colors";
+import icon from "@/constants/Icon";
 import { Header } from "react-native/Libraries/NewAppScreen";
+import Icon from "@/constants/Icon";
+
 
 export default function Layout() {
   const { clearSession, clearCredentials, user, isLoading } = useAuth0();
@@ -35,20 +39,45 @@ export default function Layout() {
   } else {
   }
 
-
   return(
-    <Stack 
-      initialRouteName="index"
-      screenOptions={{
-        header: MainHeader,
-        headerStyle:{backgroundColor: colors.lightgray.background}
-      }}
-    >
-    <Stack.Screen name="schedule" options={{header: HeaderComponent}}/>
-    <Stack.Screen name="todo" options={{header: HeaderComponent}}/>
-    {/* <Stack.Screen name="Comment" options={{header:HeaderComponent}}/> */}
-    {/* <Stack.Screen name="challenge" options={{header: HeaderComponent}}/> */}
+    <Stack screenOptions={{headerShown:false}}>
+      <Stack.Screen name="(tabs)"/>
+      <Stack.Screen name="chat"/>
     </Stack>
+    // <Stack screenOptions={{headerShown: false}}>
+    //   <Stack.Screen name="(tabs)"/>
+      //{/* <Stack.Screen name="scheduleadd" options={{header:HeaderComponent}}/>
+      // <Stack.Screen name="todoadd" options={{header:HeaderComponent}}/> */}
+    // {/* <Stack   
+    //   initialRouteName="index"
+    //   screenOptions={{
+    //     header: MainHeader,
+    //     headerStyle:{backgroundColor: colors.lightgray.background}
+    //   }}
+    // >
+    // <Drawer>
+    //   <Drawer.Screen
+    //     name="index"
+    //     options={{
+    //       drawerLabel:'Setting',
+    //       title:'overview',
+    //     }}
+    //   />
+    // <Drawer.Screen
+    //  name="index2"
+    //  options={{
+    //   drawerLabel:"Contact Us",
+    //   title: "overview",
+    //  }}
+    // />
+    // </Drawer>
+    // <Stack.Screen name="schedule" options={{header: HeaderComponent}}/>
+    // <Stack.Screen name="todo" options={{header: HeaderComponent}}/>
+    // {/* <Stack.Screen name="Comment" options={{header:HeaderComponent}}/> */}
+    // {/* <Stack.Screen name="challenge" options={{header: HeaderComponent}}/> */}
+    // </Stack>
+
+
   );
   
 }
