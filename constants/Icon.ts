3ef6a134cbf89ab9b8c9752d6673e06987bbab25
naonxj <cc4391@naver.com>
2024-05-20 04:52:09 +0900

    add Icon file

---
+++ constants/Icon.ts
@@ -0,0 +1,28 @@
+const challengeicon=require('@/assets/images/challengeicon.png');
+const scheduleicon=require('@/assets/images/scheduleicon.png');
+const todoicon= require('@/assets/images/todoicon.png');
+const modifyicon = require('@/assets/images/editicon.png');
+const deleteicon = require('@/assets/images/trashicon.png');
+const circle=require('@/assets/images/whitecircle.png');
+const listicon=require('@/assets/images/listicon.png');
+const challenge=require('@/assets/images/challenge.png');
+const diary= require('@/assets/images/diary.png');
+const exercise = require('@/assets/images/exercise.png');
+const meditation=require('@/assets/images/meditation.png');
+const water=require('@/assets/images/water.png');
+const news=require('@/assets/images/news.png');
+export default {
+    challengeicon,
+    scheduleicon,
+    todoicon,
+    modifyicon,
+    deleteicon,
+    circle,
+    listicon,
+    challenge,
+    diary,
+    exercise,
+    meditation,
+    water,
+    news,
+} 
