eb8cdaf63b58c379e6906c2a94e09f3683cec0eb
osj1405 <osj1405@naver.com>
2024-06-03 00:47:26 +0900

    store accesstoken in authStore when login

--- app/login.tsx
+++ app/login.tsx
@@ -7,18 +7,17 @@ import Logo from "@/components/Logo";
 import { useAuth0 } from "react-native-auth0";
 import { SafeAreaView } from "react-native-safe-area-context";
 import authStore from "./store/authStore";
-import { get } from "react-native/Libraries/TurboModule/TurboModuleRegistry";
 
 export default function LoginScreen() {
-  const { authorize, user, getCredentials } = useAuth0();
+  const { authorize, user } = useAuth0();
 
   const onLogin = async () => {
-    await authorize();
-    const credential = await getCredentials()
-    if(credential !== undefined)
-      authStore.setAccessToken(credential.accessToken);
+    const credentials = await authorize();
+    if(credentials){
+      authStore.setAccessToken(credentials?.accessToken)
     // doesn't run if authentication fails
     router.replace("/(tabs)");
+    }
   };
 
   return (
--- app/store/authStore.ts
+++ app/store/authStore.ts
@@ -2,13 +2,13 @@ import { makeAutoObservable } from "mobx";
 import Auth from "react-native-auth0/lib/typescript/src/auth";
 
 class AuthStore{
-    accessToken: string | undefined
+    accessToken: string | null = null;
 
     constructor(){
         makeAutoObservable(this);
     }
 
-    setAccessToken(token: string | undefined){
+    setAccessToken(token: string | null){
         this.accessToken = token;
         console.log(this.accessToken);
     }
